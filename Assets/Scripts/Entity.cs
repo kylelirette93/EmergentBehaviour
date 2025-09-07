d43a6a89eb5ca814281e01a5f597cb9790c5362a
kylelirette93 <162719550+kylelirette93@users.noreply.github.com>
2025-09-07 08:41:18 -0300

    Added rotation based on facing direction for entities

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -2,19 +2,34 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
-    float movementSpeed = 5f;
     Vector3 randomDirection;
+    float movementSpeed = 5f;
+
+    [Range(0.6f, 1.5f)]
+    [SerializeField] float visionRadius = 1.3f;
 
     private void Start()
     {
-        randomDirection = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), 0);
+        randomDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
     }
 
     private void Update()
     {
         if (randomDirection != Vector3.zero)
         {
-            transform.Translate(randomDirection * movementSpeed * Time.deltaTime);
+            // Apply random direction to the entitys position.
+            transform.position += randomDirection.normalized * movementSpeed * Time.deltaTime;
+            // Get the angle in radians based on direction and convert to degree float value.
+            float angle = Mathf.Atan2(randomDirection.y, randomDirection.x) * Mathf.Rad2Deg;
+            // Converts float value to quaternion and apply to object's forward vector.
+            transform.rotation = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
+
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, visionRadius);
     }
 }
